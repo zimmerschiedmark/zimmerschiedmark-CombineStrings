86e96e2856a9462afc7ee462ead1f91ca0eb20cd
zimmerschiedmark <zimmerschiedmark@gmail.com>
2023-02-14 20:12:10 +0000

    Completed Coding Lab Combine Strings

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -9,6 +9,7 @@ public class Lab {
      * @return the combination of strings a and b.
      */
     public String combine(String a, String b){
-        return "";
+        String newStr = a.concat(b);
+        return newStr;
     }
 }
